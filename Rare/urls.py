872adb2bf0c54254f0f90b38c2398acf3241e917
peytonkemp <peyton.kemp@gmail.com>
2021-08-18 09:41:36 -0500

    saving tags

--- Rare/urls.py
+++ Rare/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from rareapi.views import register_user, login_user
+from rareapi.views import register_user, login_user, TagView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'tags', TagView, 'tag')
 
 
 urlpatterns = [
