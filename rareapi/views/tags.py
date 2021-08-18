8095f6b4cf92c9f1ef80e3edcbd8fc748cf9c4eb
peytonkemp <peyton.kemp@gmail.com>
2021-08-18 11:14:56 -0500

    updating and destroying tags working

--- rareapi/views/tags.py
+++ rareapi/views/tags.py
@@ -72,6 +72,23 @@ class TagView(ViewSet):
         tag.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single tag
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            tag = Tag.objects.get(pk=pk)
+            tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class TagSerializer(serializers.ModelSerializer):
     """Tag model serializer returns __all__ fields
